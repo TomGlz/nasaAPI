b9b1375ec286ac916249a09d796a33e5a8ef2aba
Tomás González <101127043+TomGlz@users.noreply.github.com>
2023-01-19 19:04:16 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,8 @@ window.addEventListener('DOMContentLoaded', () => {
     fetch('https://api.nasa.gov/planetary/apod?api_key=nAGkpygV98TJGVes3QTYHqlZi4afwP17RGjTqolP')
     .then(response => response.json())
     .then(data =>{
-        image.innerHTML='<img class="w-100" src="'+data.hdurl+'">';
+        image.innerHTML='<img class="w-100" src="'+data.url+'">';
+        image.innerHTML+='<a href="'+data.hdurl+'" download><button type="button" class="btn btn-secondary mt-3">Descargar imagen HD</button></a>'
         date.innerHTML=data.date;
         title.innerHTML+='<h2>'+data.title+'</h2>';
         desc.innerHTML+='<p>'+data.explanation+'</p>';
